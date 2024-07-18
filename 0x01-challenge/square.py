06cf8c0b3e7d199efa585d6a0570a46b67d681b0
Bislon Zulu <bislonzulu1@outlook.com>
2024-07-18 12:52:10 +0200

    task 1.5

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,54 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class with methods to calculate
+the area and perimeter of the square.
+"""
 
-class Square():
+class Square:
+    """A class used to represent a Square"""
     
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the square with given width and height.
+        
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """
+        Calculate the area of the square.
+        
+        Returns:
+            int: The area of the square.
+        """
         return self.width * self.height
 
     def perimeter_of_my_square(self):
+        """
+        Calculate the perimeter of the square.
+        
+        Returns:
+            int: The perimeter of the square.
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        Return a string representation of the square.
+        
+        Returns:
+            str: The width and height of the square.
+        """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
